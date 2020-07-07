f50b58df4421a71fdb07ac729677432980f62c11
Edwin Torres <codesandtags@gmail.com>
2020-07-07 08:08:34 -0500

    feature: Added components for Menu

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -1,47 +1,12 @@
 import React from 'react';
 
 import './Homepage.scss';
+import Directory from '../../components/Directory/Directory';
 
 export const Homepage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">HATS</h1>
-                        <span className="subtitle">Shop Now</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <div className='title'>JACKETS</div>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <div className='title'>SHOES</div>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <div className='title'>WOMENS</div>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-
-                <div className='menu-item'>
-                    <div className='content'>
-                        <div className='title'>MENS</div>
-                        <span className='subtitle'>SHOP NOW</span>
-                    </div>
-                </div>
-
-            </div>
+            <Directory/>
         </div>
     )
 }
