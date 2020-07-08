8d809ae450418f75ac2e3a2aecb41db0ddede637
Edwin Torres <codesandtags@gmail.com>
2020-07-08 13:37:56 -0500

    feature: Added basic routing to app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,11 +3,19 @@ import React from 'react';
 import './App.scss';
 
 import Homepage from '../Homepage/Homepage';
+import { Route } from 'react-router-dom';
+
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
 
 function App() {
     return (
         <div className="App">
-            <Homepage></Homepage>
+            <Route exact path="/" component={Homepage} />
+            <Route path="/hats" component={HatsPage} />
         </div>
     );
 }
