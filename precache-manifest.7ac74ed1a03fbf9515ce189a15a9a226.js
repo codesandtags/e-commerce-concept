2212495a5d87175f1bd2d1442b8fc0339718b1ac
Edwin Torres <codesandtags@gmail.com>
2020-07-11 11:18:53 -0500

    Updates

---
+++ precache-manifest.7ac74ed1a03fbf9515ce189a15a9a226.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3de1d774cff0635902d46ab9c34304ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a2a317e56fe6bee9b77",
+    "url": "/static/css/main.0be3f561.chunk.css"
+  },
+  {
+    "revision": "f25db599de76e1bcdf83",
+    "url": "/static/js/2.bcabfc80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bcabfc80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a2a317e56fe6bee9b77",
+    "url": "/static/js/main.de1e7f21.chunk.js"
+  },
+  {
+    "revision": "b1b0e0eaedc2b8427f7a",
+    "url": "/static/js/runtime-main.d864ef58.js"
+  },
+  {
+    "revision": "b694607ae81fd4a9d2420003d4546644",
+    "url": "/static/media/logo-gold.b694607a.png"
+  }
+]);
