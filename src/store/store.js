be1211832fbd94cf965da6c79cea28f15dde3db8
Edwin Torres <codesandtags@gmail.com>
2020-08-31 08:52:36 -0500

    feature: Added redux-sagas to project

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,21 @@
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
+import createSagaMiddleWare from 'redux-saga';
 
 import rootReducer from './rootReducer';
 
-const middlewares = [];
+const sagaMiddleware = createSagaMiddleWare();
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares, thunk));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+// sagaMiddleware.run();
+
 export const persistor = persistStore(store);
 
 export default { store, persistor };
